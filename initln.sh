035766b79e49b7c70561689a0edb4a7ba082564f
Shawn Wilson <ag4ve.us@gmail.com>
2012-10-08 05:23:06 -0400

    initial initln script - this has just been written to motivate me to make it better - tests, messages, data structure - nothing hard, just things that come with sleep

---
+++ initln.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PWD=`pwd`
+
+ln -s $PWD/pianobar ~/.config/pianobar
+ln -s $PWD/tmux.conf ~/.tmux.conf
+ln -s $PWD/vimrc ~/.vimrc
+ln -s $PWD/Xdefaults ~/.Xdefaults
+ln -s $PWD/xinitrc ~/.xinitrc
+ln -s $PWD/xmonad.hs ~/.xmonad/xmonad.hs
+ln -s $PWD/zshenv ~/.zshenv
+ln -s $PWD/zshrc ~/.zshrc
+
