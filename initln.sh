1e32f246155a572cc12275d096b1013d53bfab31
Shawn Wilson <ag4ve.us@gmail.com>
2012-10-25 07:21:28 -0400

    create backup and copy old files there before linking to repo files

--- initln.sh
+++ initln.sh
@@ -2,12 +2,29 @@
 
 PWD=`pwd`
 
+mkdir ./backup
+
+cp -R ~/.config/pianobar ./backup
 ln -s $PWD/pianobar ~/.config/pianobar
+
+cp ~/.tmux.conf ./backup
 ln -s $PWD/tmux.conf ~/.tmux.conf
+
+cp ~/.vimrc ./backup
 ln -s $PWD/vimrc ~/.vimrc
+
+cp ~/.Xdefaults ./backup
 ln -s $PWD/Xdefaults ~/.Xdefaults
+
+cp ~/.xinitrc ./backup
 ln -s $PWD/xinitrc ~/.xinitrc
+
+cp ~/.xmonad/xmonad.hs ./backup
 ln -s $PWD/xmonad.hs ~/.xmonad/xmonad.hs
+
+cp ~/.zshenv ./backup
 ln -s $PWD/zshenv ~/.zshenv
+
+cp ~/.zshrc ./backup
 ln -s $PWD/zshrc ~/.zshrc
 
