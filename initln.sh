d8ced197cd37e5ba57a6eb695468ac6dacdd445e
Shawn Wilson <ag4ve.us@gmail.com>
2012-10-25 07:22:31 -0400

    force link

--- initln.sh
+++ initln.sh
@@ -5,26 +5,26 @@ PWD=`pwd`
 mkdir ./backup
 
 cp -R ~/.config/pianobar ./backup
-ln -s $PWD/pianobar ~/.config/pianobar
+ln -fs $PWD/pianobar ~/.config/pianobar
 
 cp ~/.tmux.conf ./backup
-ln -s $PWD/tmux.conf ~/.tmux.conf
+ln -fs $PWD/tmux.conf ~/.tmux.conf
 
 cp ~/.vimrc ./backup
-ln -s $PWD/vimrc ~/.vimrc
+ln -fs $PWD/vimrc ~/.vimrc
 
 cp ~/.Xdefaults ./backup
-ln -s $PWD/Xdefaults ~/.Xdefaults
+ln -fs $PWD/Xdefaults ~/.Xdefaults
 
 cp ~/.xinitrc ./backup
-ln -s $PWD/xinitrc ~/.xinitrc
+ln -fs $PWD/xinitrc ~/.xinitrc
 
 cp ~/.xmonad/xmonad.hs ./backup
-ln -s $PWD/xmonad.hs ~/.xmonad/xmonad.hs
+ln -fs $PWD/xmonad.hs ~/.xmonad/xmonad.hs
 
 cp ~/.zshenv ./backup
-ln -s $PWD/zshenv ~/.zshenv
+ln -fs $PWD/zshenv ~/.zshenv
 
 cp ~/.zshrc ./backup
-ln -s $PWD/zshrc ~/.zshrc
+ln -fs $PWD/zshrc ~/.zshrc
 
