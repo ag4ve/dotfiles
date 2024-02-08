1a97da2a705a85195a95295a2ecc054e425eef44
Shawn Wilson <srwilson@ioswitch.dev>
2024-02-08 15:53:46 -0500

    Configure powerline10k prompt.

---
+++ dot_zsh/rc/300-prompt.sh
@@ -0,0 +1,11 @@
+
+source ~/.zsh/powerlevel10k/powerlevel10k.zsh-theme
+(( ${+commands[direnv]} )) && emulate zsh -c "$(direnv export zsh)"
+
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+(( ${+commands[direnv]} )) && emulate zsh -c "$(direnv hook zsh)"
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.zsh/p10k.zsh ]] || source ~/.zsh/p10k.zsh
